9dd35a8426ac3f7b8ed10821a255421636c3e070
Antonio Alves <aaba03@gmail.com>
2024-11-29 13:09:32 -0300

    feat: add ShortCode decode with base62

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem "stimulus-rails"
 # Use Tailwind CSS [https://github.com/rails/tailwindcss-rails]
 gem "tailwindcss-rails"
 
+gem "rubocop"
+
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
--- app/models/short_code.rb
+++ app/models/short_code.rb
@@ -1,22 +1,31 @@
 class ShortCode
-  ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".freeze
+  ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.freeze
   BASE = ALPHABET.length
 
   def self.encode(number)
     return ALPHABET.first if number.zero? || number.nil?
 
-    result = ""
+    result = ''
 
-    while number > 0 do
+    while number > 0
       index = number % BASE
       char = ALPHABET[index]
       result.prepend char
-      number = number / BASE
+      number /= BASE
     end
-    
+
     result
   end
 
   def self.decode(string)
+    number = 0
+
+    string.reverse.each_char.with_index do |char, index|
+      power = BASE**index
+      index = ALPHABET.index(char)
+      number += index * power
+    end
+
+    number
   end
 end
